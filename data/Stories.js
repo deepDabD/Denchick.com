b8297e0c52924ad5f5f8538a2660dc95e3ac733e
deepDabD <136462246+deepDabD@users.noreply.github.com>
2023-06-21 20:40:53 +0300

    Update Stories.js

--- data/Stories.js
+++ data/Stories.js
@@ -1,4 +1,14 @@
 const stories = [
+  /*
+  гвоздика
+  
+  ,
+  {
+    name:' ',
+    cont:' '
+  }
+  
+  */
   {
     name:'Дэнчык встал со стула',
     cont:'когда то давно дэнчык напряг булки встал со стула'
@@ -7,5 +17,10 @@ const stories = [
   {
     name:'Ванильный дэнчык',
     cont:'в давние вреиена дэнчык просыпал на себя ваниль Lorem ipsum dolor, sit amet consectetur adipisicing elit. Assumenda nisi commodi molestias at cum laboriosam eos! Quis ad, enim iste neque sed quidem, temporibus ratione quod ipsam earum, debitis nemo? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Assumenda nisi commodi molestias at cum laboriosam eos! Quis ad, enim iste neque sed quidem, temporibus ratione quod ipsam earum, debitis nemo? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Assumenda nisi commodi molestias at cum laboriosam eos! Quis ad, enim iste neque sed quidem, temporibus ratione quod ipsam earum, debitis nemo? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Assumenda nisi commodi molestias at cum laboriosam eos! Quis ad, enim iste neque sed quidem, temporibus ratione quod ipsam earum, debitis nemo? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Assumenda nisi commodi molestias at cum laboriosam eos! Quis ad, enim iste neque sed quidem, temporibus ratione quod ipsam earum, debitis nemo? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Assumenda nisi commodi molestias at cum laboriosam eos! Quis ad, enim iste neque sed quidem, temporibus ratione quod ipsam earum, debitis nemo'
+  }
+  ,
+  {
+    name:'щшошозл',
+    cont:'оошшьшошшшьшьщззлзлзлзлзлзлзшошзозо'
   }
 ]
